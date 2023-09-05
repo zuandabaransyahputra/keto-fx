e142880ff6df07d6f4d10daa1cf706c8d35fbb0a
zuandabaransyahputra1998 <zuan@dooha.id>
2023-09-05 14:12:45 +0700

    edited

--- spain.php
+++ spain.php
@@ -22,7 +22,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- page title -->
-    <title>Keto Plus</title>
+    <title>Keto FX</title>
     <link rel="stylesheet" href="assets/css/slick.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/st.css">
